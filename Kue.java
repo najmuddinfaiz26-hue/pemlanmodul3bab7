7df72260bd02319c69dcd67c2d842003731e1ec6
Udeen <najmuddinfaiz26@gmail.com>
2026-04-26 16:04:45 +0700

    Add abstract Kue class with properties and methods

---
+++ Kue.java
@@ -0,0 +1,27 @@
+package Polymorfismeogii;
+
+public abstract class Kue {
+  
+    private String nama;
+    private double harga;
+
+    public Kue(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public abstract double hitungHarga();
+
+    @Override
+    public String toString() {
+        return "Nama kue: " + nama + ", Harga: Rp " + hitungHarga() + ", Harga satuan: Rp " + harga;
+    }
+}
